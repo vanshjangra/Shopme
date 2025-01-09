038ce3c27ba6911388d5345791930360d4391ad5
Vansh Jangra <vanshjangra586@gmail.com>
2025-01-09 19:03:06 +0530

    code user authentication

---
+++ src/main/java/com/shopme/controller/AccountController.java
@@ -0,0 +1,64 @@
+package com.shopme.controller;
+
+import com.shopme.entity.User;
+import com.shopme.security.ShopmeUserDetails;
+import com.shopme.service.UserService;
+import com.shopme.util.FileUploadUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+
+@Controller
+public class AccountController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/account")
+    public String viewDetails(@AuthenticationPrincipal ShopmeUserDetails loggedUser,
+                              Model model){
+        String email = loggedUser.getUsername();
+        User user = service.getByEmail(email);
+        model.addAttribute("user",user);
+
+        return "account_form";
+    }
+
+    @PostMapping("/account/update")
+    public String saveDetails (User user, RedirectAttributes redirectAttributes,
+                           @AuthenticationPrincipal ShopmeUserDetails loggerUser,
+                           @RequestParam("image") MultipartFile multipartFile) throws IOException {
+//        System.out.println(user);
+//        System.out.println(multipartFile.getOriginalFilename());
+
+        if (!multipartFile.isEmpty()){
+            String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+            user.setPhotos(fileName);
+            User savedUser = service.updateAccont(user);
+
+            String uploadDir = "user-photos/" + savedUser.getId();
+
+            FileUploadUtil.cleanDir(uploadDir);
+            FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+        }
+        else {
+            if (user.getPhotos().isEmpty()) user.setPhotos(null);
+            service.updateAccont(user);
+        }
+
+        loggerUser.setFirstName(user.getFirstName());
+        loggerUser.setLastName(user.getLastName());
+
+        redirectAttributes.addFlashAttribute("message", "Your account details have been updated.");
+
+        return "redirect:/account";
+    }
+}
---
+++ src/main/java/com/shopme/security/ShopmeUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.shopme.security;
+
+import com.shopme.dao.UserRepository;
+import com.shopme.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class ShopmeUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.getUserByEmail(email);
+        if (user != null){
+            return new ShopmeUserDetails(user);
+        }
+
+        throw new UsernameNotFoundException("Could not find user with email: " + email);
+    }
+}
